5b64dcfcb14d5667d3394f68b78efcb272c91e07
Nicolas Oga <2112.oga@gmail.com>
2020-07-26 18:24:05 -0300

    Use deployed service

--- src/app/services/account-validation.service.ts
+++ src/app/services/account-validation.service.ts
@@ -6,6 +6,6 @@ export class AccountValidationService {
   constructor(private http: HttpClient) { }
 
   validateAccount(email: string) {
-    return this.http.post('http://localhost:3000/send-account-validation-email', { email }).subscribe()
+    return this.http.post('http://prueba-teamknowlogy.nicolasoga.com.ar/send-account-validation-email', { email }).subscribe()
   }
 }
